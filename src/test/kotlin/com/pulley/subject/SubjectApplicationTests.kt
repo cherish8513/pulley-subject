4954a1fb20fb6713c0509e44d0d563187fa5b132
jooheon <cherish8513@naver.com>
2024-11-13 20:24:35 +0900

    initialize project

---
+++ src/test/kotlin/com/pulley/subject/SubjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pulley.subject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SubjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
